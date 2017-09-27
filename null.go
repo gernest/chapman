84d2cbade81bc0adbffd66b522adfcc904403a7c
gernest <geofreyernest@live.com>
2017-09-27 14:29:27 +0300

    Add nullLexer

---
+++ null.go
@@ -0,0 +1,63 @@
+package goes
+
+import (
+	"bytes"
+	"fmt"
+)
+
+type nullLexer struct{}
+
+func (nullLexer) name() string {
+	return "null"
+}
+
+func (nullLexer) accept(s scanner) bool {
+	n, _, err := s.peek()
+	if err != nil {
+		return false
+	}
+	var b bytes.Buffer
+	if n == 'n' {
+		b.WriteRune(n)
+		for i := 2; i < 5; i++ {
+			n, _, err := s.peekAt(i)
+			if err != nil {
+				return false
+			}
+			b.WriteRune(n)
+		}
+		fmt.Println(&b)
+		return b.String() == "null"
+	}
+	return false
+}
+
+func (n nullLexer) lex(s scanner, ctx *context) (*token, error) {
+	var start, end position
+	if ctx.lastToken != nil {
+		start, end = ctx.lastToken.End, start
+	}
+	nx, w, err := s.next()
+	if err != nil {
+		return nil, err
+	}
+	chrs := string(nx)
+	end.Column += w
+	for i := 0; i < 3; i++ {
+		nx, w, err = s.next()
+		if err != nil {
+			return nil, err
+		}
+		end.Column += w
+		chrs += string(nx)
+	}
+	if chrs == "null" {
+		return &token{
+			Text:  chrs,
+			Kind:  NULL,
+			Start: start,
+			End:   end,
+		}, nil
+	}
+	return nil, fmt.Errorf(unexpectedTkn, n.name(), end)
+}
---
+++ null_test.go
@@ -0,0 +1,24 @@
+package goes
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestNullLexer(t *testing.T) {
+	var l nullLexer
+	sample := []string{"null", "null "}
+	for _, v := range sample {
+		s := newBufioScanner(strings.NewReader(v))
+		if !l.accept(s) {
+			t.Error("expected to accept", v)
+		}
+		tk, err := l.lex(s, &context{})
+		if err != nil {
+			t.Fatal(err)
+		}
+		if tk.Text != v {
+			t.Errorf("expected %s got %s", v, tk.Text)
+		}
+	}
+}
